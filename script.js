c2f33f217e1e463bcaec2cca600d09138820689a
Abhay vishwakarma <akvishwakarma25082002@gmail.in>
2025-03-02 23:39:39 +0530

    mode change

---
+++ script.js
@@ -0,0 +1,14 @@
+let btn=document.querySelector(".btn");
+let currmode="light";
+
+btn.addEventListener("click",() =>{
+
+    if(currmode==="light"){
+        currmode="dark";
+        document.querySelector("body").style.backgroundColor="blue";
+    }else{
+        currmode="light";
+        document.querySelector("body").style.backgroundColor="green";
+    }
+    console.log(currmode);
+});
